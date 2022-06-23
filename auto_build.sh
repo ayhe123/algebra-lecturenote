b00825bb4af66326da125a2362fb46eff570adde
ayhe123 <1051065690@qq.com>
2022-06-23 16:43:42 +0800

    加入了删除PDF文件的功能

--- auto_build.sh
+++ auto_build.sh
@@ -6,10 +6,11 @@ while true; do
 0: 编译
 1: 清理临时文件
 2: 编译且清理临时文件
-3: 退出
+3: 删除所有的 PDF 文件 (慎用)
+4: 退出
 "
-    read -p "输入 0-3 间的数字 > "
-    if [[ $REPLY =~ ^[0-3]$ ]]; then
+    read -p "输入 0-4 间的数字 > "
+    if [[ $REPLY =~ ^[0-4]$ ]]; then
         if [[ $REPLY =~ ^[02]$ ]]; then
             rm __latexindent_temp.tex
             echo "开始编译"
@@ -23,6 +24,13 @@ while true; do
             echo "开始清理"
             rm __latexindent_temp.tex *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex\(busy\) *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
             echo "清理完成"
+        fi
+        if [[ $REPLY =~ ^3$ ]]; then
+            read -p "是否删除当前目录下所有的 PDF 文件? (y/[n]) > "
+            if [[ $REPLY =~ ^y$ ]]; then
+                rm *.pdf
+                echo "清理完成"
+            fi
         fi
         exit 0
     fi
